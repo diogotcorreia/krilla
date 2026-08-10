f6b6d94c2188ef6e093f19724ae2d4075258143b
Diogo Correia <me@diogotc.com>
2026-08-10 14:10:19 +0100

    Add radio field

--- crates/krilla/src/interactive/forms.rs
+++ crates/krilla/src/interactive/forms.rs
@@ -7,7 +7,6 @@ use pdf_writer::{writers::Form, Finish, Ref, TextStr};
 use crate::{
     annotation::{DualStateAppearance, SimpleAppearance, WidgetAnnotation},
     chunk_container::ChunkContainer,
-    forms::kind::PushButton,
     geom::Rect,
     serialize::SerializeContext,
     stream::Stream,
@@ -101,6 +100,14 @@ impl FormField<kind::Checkbox> {
         }
     }
 
+    pub fn set_checked(&mut self, checked: bool) {
+        self.kind.checked = Some(checked);
+    }
+
+    pub fn set_default_checked(&mut self, checked: bool) {
+        self.kind.default_checked = Some(checked);
+    }
+
     pub fn new_widget(
         &self,
         rect: Rect,
@@ -113,6 +120,41 @@ impl FormField<kind::Checkbox> {
             "Off".to_string(),
             off_appearance,
             "Yes".to_string(),
+            on_appearance,
+        )
+    }
+}
+
+#[allow(missing_docs)]
+impl FormField<kind::Radio> {
+    pub fn radio(name: String) -> Self {
+        Self {
+            name,
+            ..Default::default()
+        }
+    }
+
+    pub fn set_value(&mut self, value: String) {
+        self.kind.value = Some(value);
+    }
+
+    pub fn set_default_value(&mut self, value: String) {
+        self.kind.default_value = Some(value);
+    }
+
+    pub fn new_widget(
+        &self,
+        rect: Rect,
+        value: String,
+        off_appearance: Stream,
+        on_appearance: Stream,
+    ) -> WidgetAnnotation<DualStateAppearance> {
+        WidgetAnnotation::dual(
+            self.name.clone(),
+            rect,
+            "Off".to_string(),
+            off_appearance,
+            value,
             on_appearance,
         )
     }
@@ -142,6 +184,7 @@ impl<T: SerializableField> FormField<T> {
 pub enum FieldKind {
     PushButton(FormField<kind::PushButton>),
     Checkbox(FormField<kind::Checkbox>),
+    Radio(FormField<kind::Radio>),
 }
 
 impl FieldKind {
@@ -155,6 +198,7 @@ impl FieldKind {
         match self {
             Self::PushButton(f) => f.serialize_field(sc, chunk_container, root_ref, annotations),
             Self::Checkbox(f) => f.serialize_field(sc, chunk_container, root_ref, annotations),
+            Self::Radio(f) => f.serialize_field(sc, chunk_container, root_ref, annotations),
         }
     }
 
@@ -162,6 +206,7 @@ impl FieldKind {
         match self {
             Self::PushButton(f) => &f.name,
             Self::Checkbox(f) => &f.name,
+            Self::Radio(f) => &f.name,
         }
     }
 }
@@ -171,7 +216,10 @@ trait SerializableField {
 }
 
 mod kind {
-    use pdf_writer::types::{CheckBoxState, FieldFlags};
+    use pdf_writer::{
+        types::{CheckBoxState, FieldFlags},
+        Name,
+    };
 
     use super::SerializableField;
 
@@ -190,8 +238,8 @@ mod kind {
     #[allow(missing_docs)]
     #[derive(Debug, Clone, Default)]
     pub struct Checkbox {
-        checked: Option<bool>,
-        default_checked: Option<bool>,
+        pub(super) checked: Option<bool>,
+        pub(super) default_checked: Option<bool>,
     }
 
     impl SerializableField for Checkbox {
@@ -201,10 +249,43 @@ mod kind {
             } else {
                 CheckBoxState::Off
             };
+            let default_value = if self.default_checked.unwrap_or(false) {
+                CheckBoxState::Yes
+            } else {
+                CheckBoxState::Off
+            };
 
             field
                 .field_type(pdf_writer::types::FieldType::Button)
-                .checkbox_value(value);
+                .checkbox_value(value)
+                .checkbox_default_value(default_value);
+        }
+    }
+
+    #[allow(missing_docs)]
+    #[derive(Debug, Clone, Default)]
+    pub struct Radio {
+        pub(super) value: Option<String>,
+        pub(super) default_value: Option<String>,
+    }
+
+    impl SerializableField for Radio {
+        fn serialize_field<'a>(&self, field: &mut pdf_writer::writers::Field<'a>) {
+            let value = if let Some(value) = &self.value {
+                value.as_bytes()
+            } else {
+                b"Off"
+            };
+            let default_value = if let Some(value) = &self.default_value {
+                value.as_bytes()
+            } else {
+                b"Off"
+            };
+
+            field
+                .field_type(pdf_writer::types::FieldType::Button)
+                .radio_value(Name(value))
+                .radio_default_value(Name(default_value));
         }
     }
 }
@@ -218,5 +299,11 @@ impl From<FormField<kind::PushButton>> for FieldKind {
 impl From<FormField<kind::Checkbox>> for FieldKind {
     fn from(value: FormField<kind::Checkbox>) -> Self {
         Self::Checkbox(value)
+    }
+}
+
+impl From<FormField<kind::Radio>> for FieldKind {
+    fn from(value: FormField<kind::Radio>) -> Self {
+        Self::Radio(value)
     }
 }
